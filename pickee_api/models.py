d196e352bf0f22a4552dae72b2c2629d35fcafaa
Rhys <rhys-stephens@hotmail.co.uk>
2020-03-09 16:54:37 +0000

    Initial UserProfile model created

--- pickee_api/models.py
+++ pickee_api/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    user.first_name.blank = False
+    user.last_name.blank = False
+
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    GENDER_CHOICES = ('Male', 'Female', 'Other', 'Prefer not to say')
+    gender = models.CharField(max_length=18, choices=GENDER_CHOICES)
+    age = models.IntegerField(validators=[MaxValueValidator(100), MinValueValidator(0)])
+    associated_users = models.ManyToManyField("self")
+
+    favourite_actors = models.ManyToManyField(Actor, symmetrical=False)
+    favourite_movies = models.ManyToManyField(Movie, symmetrical=False)
+    favourite_genres = models.ManyToManyField(Genre, symmetrical=False)
+
+    def __str__(self):
+        return self.user.first_name + " " + self.user.last_name
+
+class Actor(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    picture = models.ImageField()
+    #need some way to retrieve from an API
+
+class Movie(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    picture = models.ImageField()
+    #need some way to retrieve from an API
+
+class Genre(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    #need some way to retrieve from an API
