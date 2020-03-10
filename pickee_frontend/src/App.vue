f8ac49428a835f611f76af648e8547e98857cafa
Anton Samoilov <absamoilov88@gmail.com>
2020-03-10 22:21:01 +0000

    Update demo markup

--- pickee_frontend/src/App.vue
+++ pickee_frontend/src/App.vue
@@ -76,19 +76,63 @@
                         <TimeSlider />
                     </div>
                 </div>
+
+                <div class="mb-10">
+                    <v-dialog v-model="dialog" width="500">
+                        <template v-slot:activator="{ on }">
+                            <v-btn color="primary" v-on="on">
+                                Click Me
+                            </v-btn>
+                        </template>
+
+                        <v-card light>
+                            <div class="pa-6">
+                                <v-icon
+                                    class="dialog-close"
+                                    large
+                                    color="black"
+                                    @click="dialog = false"
+                                    >{{ iconClose }}</v-icon
+                                >
+                                <v-card-text class="mt-6">
+                                    Lorem ipsum dolor sit amet, consectetur
+                                    adipiscing elit, sed do eiusmod tempor
+                                    incididunt ut labore et dolore magna aliqua.
+                                    Ut enim ad minim veniam, quis nostrud
+                                    exercitation ullamco laboris nisi ut aliquip
+                                    ex ea commodo consequat.
+                                </v-card-text>
+
+                                <v-card-actions class="dialog-actions">
+                                    <div class="mb-4">
+                                        <v-btn
+                                            block
+                                            color="primary"
+                                            @click="dialog = false"
+                                            >Manage account
+                                        </v-btn>
+                                    </div>
+                                    <v-btn block outlined>Logout </v-btn>
+                                </v-card-actions>
+                            </div>
+                        </v-card>
+                    </v-dialog>
+                </div>
             </v-content>
         </v-app>
     </div>
 </template>
 
 <script>
-    import { mdiChevronRight, mdiPlusCircle } from "@mdi/js";
+    import { mdiChevronRight, mdiPlusCircle, mdiClose } from "@mdi/js";
     import TimeSlider from "./components/TimeSlider/TimeSlider.vue";
 
     export default {
         name: "AppOne",
         data: () => ({
+            dialog: false,
             first: "belfdev@gmail.com",
+            iconClose: mdiClose,
             iconPlus: mdiPlusCircle,
             iconArrowRight: mdiChevronRight
         }),
