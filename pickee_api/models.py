857209c0684aea181f95f2a37822fa1828b26cdc
Rhys <rhys-stephens@hotmail.co.uk>
2020-03-10 09:36:27 +0000

    Created recommendation and session models

--- pickee_api/models.py
+++ pickee_api/models.py
@@ -12,7 +12,7 @@ class UserProfile(models.Model):
     GENDER_CHOICES = ('Male', 'Female', 'Other', 'Prefer not to say')
     gender = models.CharField(max_length=18, choices=GENDER_CHOICES)
     age = models.IntegerField(validators=[MaxValueValidator(100), MinValueValidator(0)])
-    associated_users = models.ManyToManyField("self")
+    associated_users = models.ManyToManyField('self')
 
     def __str__(self):
         return self.user.first_name + " " + self.user.last_name
@@ -23,13 +23,43 @@ class FavouriteActors(models.Model):
     picture = models.ImageField()
     #need some way to retrieve from an API
 
+    def __str__(self):
+        return self.name
+
 class FavouriteMovies(models.Model):
     user = models.ForeignKey('UserProfile', on_delete=models.CASCADE)
-    name = models.CharField(max_length=128, unique=True)
-    picture = models.ImageField()
-    #need some way to retrieve from an API
+    movie = models.ForeignKey('Movie', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user + ": " + self.movie
 
 class FavouriteGenres(models.Model):
     user = models.ForeignKey('UserProfile', on_delete=models.CASCADE)
-    name = models.CharField(max_length=128, unique=True)
+    name = models.CharField(max_length=128)
     #need some way to retrieve from an API
+
+    def __str__(self):
+        return self.name
+
+class Movie(models.Model):
+    name = models.CharField(max_length=128)
+    picture = models.ImageField()
+    #need some way to retrieve from an API
+
+    def __str__(self):
+        return self.name
+
+class Recommendation(models.Model):
+    movie = models.ForeignKey('Movie', on_delete=models.CASCADE)
+    session = models.ForeignKey('Session', on_delete=models.CASCADE)
+    USER_CHOICES = ('Accepted', 'Rejected', 'Bookmarked')
+    user_choice = models.CharField(max_length=10, choices=USER_CHOICES)
+
+    def __str__(self):
+        return self.movie
+
+class Session(models.Model):
+    users = models.ManyToManyField('UserProfile')
+
+    def __str__(self):
+        return self.id
