ef4aee0766a434cafc80532c74edd54105576d4f
Anton Samoilov <absamoilov88@gmail.com>
2020-03-10 21:49:22 +0000

    Add time slider component (WIP)

---
+++ pickee_frontend/src/components/TimeSlider/TimeSlider.js
@@ -0,0 +1,20 @@
+export default {
+    computed: {
+        timePretty: function() {
+            let text;
+            const mins = this.minutes;
+
+            if (mins < 60) {
+                text = `${mins} minutes`;
+            } else {
+                const h = mins / 60;
+                const pl = h > 1 ? "s" : "";
+                text = `${h} hour${pl}`;
+            }
+            return text;
+        }
+    },
+    data: () => ({
+        minutes: 30
+    })
+};
---
+++ pickee_frontend/src/components/TimeSlider/TimeSlider.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="time-slider">
+        <span class="time-slider__label">{{ timePretty }}</span>
+        <div class="time-slider__input">
+            <v-slider
+                v-model="minutes"
+                step="30"
+                min="30"
+                max="240"
+                ticks
+            ></v-slider>
+        </div>
+    </div>
+</template>
+
+<script src="./TimeSlider.js"></script>
+<style src="./TimeSlider.css"></style>
