91dcc659b37c0e94494e5db6e9b404135840c45d
nathanieldschneider <nathanieldanielschneider@gmail.com>
2020-03-11 16:17:05 +0000

    Create login box styling

---
+++ pickee_frontend/src/components/FormTextField/FormTextField.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="form-text-field">
+        <label class="label">{{ label }}</label>
+        <v-text-field solo dark></v-text-field>
+    </div>
+</template>
+
+<script src="./FormTextField.js" />
+<style src="./FormTextField.css" />
+
