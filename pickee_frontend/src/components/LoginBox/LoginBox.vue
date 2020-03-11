0679ce85f746df8bf4d7dd31b1f28bed48ad3a20
nathanieldschneider <nathanieldanielschneider@gmail.com>
2020-03-11 16:16:11 +0000

    Create login box vue

---
+++ pickee_frontend/src/components/LoginBox/LoginBox.vue
@@ -0,0 +1,12 @@
+<template>
+    <v-card class="login-box" light>
+        <h1 class="login-box__header">LOGIN</h1>
+        <FormTextField label="Username" />
+        <FormTextField label="Password" />
+        <v-btn class="login-box__submit" color="primary">Login</v-btn>
+        <a class="login-box__signup-link" href="#">Create Account</a>
+    </v-card>
+</template>
+
+<script src="./LoginBox.js" />
+<style src="./LoginBox.css" />
