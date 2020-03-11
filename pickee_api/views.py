e0b85f8c1919ed07a388360966ade79fb3114e35
Pedro Belfort <belfortdev@gmail.com>
2020-03-11 14:10:09 +0000

    Set up basic user sign up

--- pickee_api/views.py
+++ pickee_api/views.py
@@ -1,15 +1,17 @@
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 
 # Temporary renders login.html
 # TODO: replace this by the Login Vue App
 def user_login(request):
     if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(username=username, password=password)
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
 
         if user:
             if user.is_active:
@@ -27,5 +29,15 @@ def user_login(request):
 # Temporary renders signup.html
 # TODO: replace this by the SignUp Vue App
 def user_signup(request):
-    response = render(request, 'signup.html', context={})
-    return response
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
