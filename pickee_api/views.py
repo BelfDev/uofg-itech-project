2e0165d82a9308131201daaf3f79bfb14663e554
Pedro Belfort <belfortdev@gmail.com>
2020-03-11 12:09:07 +0000

    Set up basic user login

--- pickee_api/views.py
+++ pickee_api/views.py
@@ -1,15 +1,31 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
 
 # Temporary renders login.html
 # TODO: replace this by the Login Vue App
-def login(request):
-    response = render(request, 'login.html', context={})
-    return response
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return redirect('index')
+            else:
+                return HttpResponse("Your account is disabled.")
+        else:
+            print(f"Invalid login details: {username}, {password}")
+            return HttpResponse("Invalid login details supplied.")
+    else:
+        return render(request, 'login.html')
 
 
 # Temporary renders signup.html
 # TODO: replace this by the SignUp Vue App
-def signup(request):
+def user_signup(request):
     response = render(request, 'signup.html', context={})
     return response
--- pickee_project/urls.py
+++ pickee_project/urls.py
@@ -7,8 +7,8 @@ page_urls = [
     path("", TemplateView.as_view(template_name="index.html"), name="index"),
     path("vue_app_01/", TemplateView.as_view(template_name="vue_app_01.html"), name="vue_app_01"),
     path("vue_app_02/", TemplateView.as_view(template_name="vue_app_02.html"), name="vue_app_02"),
-    path("login/", views.login, name='login'),
-    path("signup/", views.signup, name='signup')
+    path("login/", views.user_login, name='login'),
+    path("signup/", views.user_signup, name='signup')
 ]
 
 urlpatterns = [
