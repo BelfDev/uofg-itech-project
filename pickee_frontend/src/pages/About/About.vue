631d0cae2e443ef67c8d90cd3aa22a207e49b82b
nathanieldschneider <nathanieldanielschneider@gmail.com>
2020-03-11 18:44:23 +0000

    Add content for about page

--- pickee_frontend/src/pages/About/About.vue
+++ pickee_frontend/src/pages/About/About.vue
@@ -8,7 +8,16 @@
                     <PersonList :developers="developers"/>
                     <div class="about-page__text">
                         <h2>MadStax</h2>
-                        <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of “de Finibus Bonorum et Malorum” (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, “Lorem ipsum dolor sit amet..”, comes from a line in section 1.10.32.</p>
+                        <p>MadStax is a team of developers established in 2019 at a Hackathon they
+                           didn't finish. It consists of two top tier developers, a bridge engineering 
+                           prodigy, and a man from England. Currently we have a number of successful
+                           projects completed, including a Top Trumps game so perfect we were sued 
+                           by CD Projekt Red for plagiarizing Gwent. We are currently working on
+                           expanding Pickee for a global roll-out and hope to compete with the titans
+                           of industry: Alphabet, Apple, Blockbuster, etc. While we are currently based
+                           in Glasgow, we are expecting to open our Singapore offices by the end of next
+                           week, followed by Sydney, Hong Kong, San Francisco, and Milton Keynes.
+                        </p>
                     </div>
                 </div>
             </v-content>
