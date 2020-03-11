913529d7c3c4849a1879070ca773c561bfeba891
nathanieldschneider <nathanieldanielschneider@gmail.com>
2020-03-11 13:22:43 +0000

    Add login URL

--- pickee_project/urls.py
+++ pickee_project/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     path('api/', include('pickee_api.urls')),
     path("", TemplateView.as_view(template_name="index.html"), name="index"),
-    path("vue_app_01/", TemplateView.as_view(template_name="vue_app_01.html"), name="vue_app_01"),
-    path("vue_app_02/", TemplateView.as_view(template_name="vue_app_02.html"), name="vue_app_02"),
+    path("vue_app_01/", TemplateView.as_view(template_name="vue_app_01.html"),
+         name="vue_app_01"),
+    path("login/", TemplateView.as_view(template_name="login.html"), name="login"),
 ]
