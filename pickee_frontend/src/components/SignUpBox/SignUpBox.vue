6291adbc714d3eca0ad9721d7eb4295852426c27
nathanieldschneider <nathanieldanielschneider@gmail.com>
2020-03-11 16:32:05 +0000

    Create sign up component

---
+++ pickee_frontend/src/components/SignUpBox/SignUpBox.vue
@@ -0,0 +1,11 @@
+<template>
+    <v-card class="auth-box" light>
+        <h1 class="auth-box__header">Sign Up</h1>
+        <FormTextField label="Username" />
+        <FormTextField label="Password" />
+        <v-btn class="auth-box__submit" color="primary">Create Account</v-btn>
+        <a class="auth-box__signup-link" href="#">Login</a>
+    </v-card>
+</template>
+
+<script src="./SignUpBox.js" />
