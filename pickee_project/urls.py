f721fc68e9aaa198cc2fb6dcbb4d4a6825cdd274
Pedro Belfort <belfortdev@gmail.com>
2020-03-11 11:55:58 +0000

    Organize project URL categories: admin | api | pages

--- pickee_project/urls.py
+++ pickee_project/urls.py
@@ -1,27 +1,17 @@
-"""pickee_project URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from pickee_api import views
 
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    path('api/', include('pickee_api.urls')),
+page_urls = [
     path("", TemplateView.as_view(template_name="index.html"), name="index"),
     path("vue_app_01/", TemplateView.as_view(template_name="vue_app_01.html"), name="vue_app_01"),
     path("vue_app_02/", TemplateView.as_view(template_name="vue_app_02.html"), name="vue_app_02"),
+    path("login/", views.login, name='login'),
+    path("signup/", views.signup, name='signup')
 ]
+
+urlpatterns = [
+                  path('admin/', admin.site.urls),
+                  path('api/', include('pickee_api.urls')),
+              ] + page_urls
