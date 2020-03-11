bfaeba538ce2d5b7a5288cbff3023e33df4ddfcf
nathanieldschneider <nathanieldanielschneider@gmail.com>
2020-03-11 16:33:57 +0000

    Update login box to utilise auth box general style

--- pickee_frontend/src/components/LoginBox/LoginBox.vue
+++ pickee_frontend/src/components/LoginBox/LoginBox.vue
@@ -1,12 +1,11 @@
 <template>
-    <v-card class="login-box" light>
-        <h1 class="login-box__header">LOGIN</h1>
+    <v-card class="auth-box" light>
+        <h1 class="auth-box__header">Login</h1>
         <FormTextField label="Username" />
         <FormTextField label="Password" />
-        <v-btn class="login-box__submit" color="primary">Login</v-btn>
-        <a class="login-box__signup-link" href="#">Create Account</a>
+        <v-btn class="auth-box__submit" color="primary">Login</v-btn>
+        <a class="auth-box__signup-link" href="#">Create Account</a>
     </v-card>
 </template>
 
 <script src="./LoginBox.js" />
-<style src="./LoginBox.css" />
