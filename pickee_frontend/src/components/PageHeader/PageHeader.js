e313b0c16bcaabce40195da0284035585b243d86
nathanieldschneider <nathanieldanielschneider@gmail.com>
2020-03-11 14:37:20 +0000

    Connected page header js

--- pickee_frontend/src/components/PageHeader/PageHeader.js
+++ pickee_frontend/src/components/PageHeader/PageHeader.js
@@ -1,5 +1,12 @@
 import Navigation from "@/components/Navigation/Navigation.vue";
+import HeaderAuth from "@/components/HeaderAuth/HeaderAuth.vue";
+import Logo from "@/components/Logo/Logo.vue";
+
 export default {
     name: "PageHeader",
-    components: {Navigation}
+    components: {
+        Navigation, 
+        HeaderAuth, 
+        Logo
+    }
 }
