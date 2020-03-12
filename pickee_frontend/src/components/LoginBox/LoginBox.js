87f3ab2b684d1b1fb661287f6a7cf4df1c2c6050
Anton Samoilov <absamoilov88@gmail.com>
2020-03-12 23:44:06 +0000

    Update login box component

--- pickee_frontend/src/components/LoginBox/LoginBox.js
+++ pickee_frontend/src/components/LoginBox/LoginBox.js
@@ -2,6 +2,12 @@ import FormTextField from "@/components/FormTextField/FormTextField.vue";
 
 export default {
     name: "LoginBox",
+    data: function() {
+        return {
+            token: this.$cookies.get("csrftoken")
+        }
+    },
+    props: ['formUrl'],
     components: {
         FormTextField
     }
