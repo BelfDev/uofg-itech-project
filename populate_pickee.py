db0a2ea77065e29796403bba87492445503673a0
Rhys <rhys-stephens@hotmail.co.uk>
2020-03-14 18:29:52 +0000

    created skeletons for population script

--- populate_pickee.py
+++ populate_pickee.py
@@ -6,3 +6,43 @@ from pickee_api.models import (UserProfile,FavoriteActor,Actor,
                                 FavoriteMovie,Movie,MovieCast,
                                 FavoriteGenre,Genre,Recommendation,
                                 Session)
+
+users = [
+    {}
+]
+
+favorite_actors = [
+    {}
+]
+
+actors = [
+    {}
+]
+
+favorite_movies = [
+    {}
+]
+
+movies = [
+    {}
+]
+
+movie_casts = [
+    {}
+]
+
+favorite_genres = [
+    {}
+]
+
+genres = [
+    {}
+]
+
+recommendations = [
+    {}
+]
+
+sessions = [
+    {}
+]
