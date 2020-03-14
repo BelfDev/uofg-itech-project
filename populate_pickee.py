5793ff9068b8f5b2fa2cec0d454b3c48b21f8067
Rhys <rhys-stephens@hotmail.co.uk>
2020-03-14 18:40:10 +0000

    Created some actor instances

--- populate_pickee.py
+++ populate_pickee.py
@@ -16,7 +16,13 @@ favorite_actors = [
 ]
 
 actors = [
-    {}
+    {'person_id':31,'name':'Tom Hanks','picture':'null'},
+    {'person_id':6193,'name':'Leonardo DiCaprio','picture':'null'},
+    {'person_id':74568,'name':'Chris Hemsworth','picture':'null'},
+    {'person_id':4491,'name':'Jennifer Aniston','picture':'null'},
+    {'person_id':11701,'name':'Angelina Jolie','picture':'null'},
+    {'person_id':84223,'name':'Anna Kendrick','picture':'null'},
+    {'person_id':192,'name':'Morgan Freeman','picture':'null'}
 ]
 
 favorite_movies = [
