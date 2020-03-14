195c99af5575b018b463b97d939cf24dbab58947
nathanieldschneider <nathanieldanielschneider@gmail.com>
2020-03-14 17:13:22 +0000

    Add main profile page and styling

--- pickee_frontend/src/pages/ProfileMain/ProfileMain.vue
+++ pickee_frontend/src/pages/ProfileMain/ProfileMain.vue
@@ -2,7 +2,12 @@
     <v-app>
         <div class="profile-page">
             <ProfileNavDrawer />
-            <PageHeader />
+            <main class="profile-page-main">
+                <PageHeader />
+                <v-content class="profile-page-content">
+                <ProfileHeader />
+                </v-content>
+            </main>
         </div>
     </v-app>
 </template>
@@ -10,12 +15,13 @@
 <script>
     import PageHeader from "@/components/PageHeader/PageHeader.vue";
     import ProfileNavDrawer from "@/components/ProfileNavDrawer/ProfileNavDrawer.vue";
+    import ProfileHeader from "@/components/ProfileHeader/ProfileHeader.vue";
 
     export default {
         name: "ProfileMain",
         data: function() {
             return {};
         },
-        components: { PageHeader, ProfileNavDrawer }
+        components: { PageHeader, ProfileNavDrawer, ProfileHeader }
     };
 </script>
