383d384a509c966dd5bf4ea32bde2f726d1b77e7
Anton Samoilov <absamoilov88@gmail.com>
2020-03-14 18:50:29 +0000

    Pass urls and data to vue instance

--- pickee_frontend/src/pages/SignUp/SignUp.js
+++ pickee_frontend/src/pages/SignUp/SignUp.js
@@ -3,7 +3,9 @@ import SignUp from "./SignUp.vue";
 
 import vuetify from "@/plugins/vuetify/vuetify";
 import Responsive from "@/plugins/responsive";
+import VueCookies from 'vue-cookies'
 
+Vue.use(VueCookies);
 new Responsive();
 
 import "@/assets/styles/app.css";
