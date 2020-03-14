c93594096f543ad89b0192717253c9e01429f070
nathanieldschneider <nathanieldanielschneider@gmail.com>
2020-03-14 17:15:06 +0000

    Add profile preferences page

--- pickee_frontend/src/pages/ProfilePreferences/ProfilePreferences.vue
+++ pickee_frontend/src/pages/ProfilePreferences/ProfilePreferences.vue
@@ -1,15 +1,21 @@
 <template>
     <v-app>
-      
+        <div class="profile-page">
+            <ProfileNavDrawer />
+            <PageHeader />
+        </div>
     </v-app>
 </template>
 
 <script>
+    import PageHeader from "@/components/PageHeader/PageHeader.vue";
+    import ProfileNavDrawer from "@/components/ProfileNavDrawer/ProfileNavDrawer.vue";
 
-export default {
-    name: "ProfilePreferences",
-    data: function() {
-        return {};
-    },
-};
+    export default {
+        name: "ProfilePreferences",
+        data: function() {
+            return {};
+        },
+        components: { PageHeader, ProfileNavDrawer }
+    };
 </script>
