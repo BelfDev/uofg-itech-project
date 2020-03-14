22d859ee832604539bb32875a3b8fac87d21df8e
Pedro Belfort <belfortdev@gmail.com>
2020-03-14 21:15:32 +0000

    Register models in the admin site

--- pickee_api/admin.py
+++ pickee_api/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from pickee_api import models
 
-# Register your models here.
+# Registers Pickee API models in the admin site
+admin.site.register(models.UserProfile)
+admin.site.register(models.FavoriteActor)
+admin.site.register(models.Actor)
+admin.site.register(models.FavoriteMovie)
+admin.site.register(models.Movie)
+admin.site.register(models.MovieCast)
+admin.site.register(models.FavoriteGenre)
+admin.site.register(models.Genre)
+admin.site.register(models.Recommendation)
+admin.site.register(models.Session)
