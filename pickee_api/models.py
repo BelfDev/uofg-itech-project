d8ca462338af4b71e0b818cfb00d8c214ed7c088
Rhys <rhys-stephens@hotmail.co.uk>
2020-03-14 11:43:12 +0000

    Altered user model back to standard user due to authentication clashes

--- pickee_api/models.py
+++ pickee_api/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
 
-class UserProfile(AbstractUser):
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     email = models.EmailField(max_length=300, unique=True, blank=False)
     first_name = models.CharField(max_length=36, blank=False)
     last_name = models.CharField(max_length=36, blank=False)
