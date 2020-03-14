764b15c72a4441fe9920f6e66fa7e9d5916da44a
nathanieldschneider <nathanieldanielschneider@gmail.com>
2020-03-14 17:13:51 +0000

    Add ProfileNavDrawer component trifecta

--- pickee_frontend/src/components/ProfileNavDrawer/ProfileNavDrawer.js
+++ pickee_frontend/src/components/ProfileNavDrawer/ProfileNavDrawer.js
@@ -1,14 +1,32 @@
 import Logo from "@/components/Logo/Logo.vue";
 import { mdiAccountBox, mdiCog, mdiFilmstrip } from "@mdi/js"
+import NavDrawerLinks from "@/components/NavDrawerLinks/NavDrawerLinks.vue"
 
 export default {
     name: "ProfileNavDrawer",
     data: () => ({
-        iconProfile: mdiAccountBox,
-        iconPreferences: mdiCog,
-        iconHistory: mdiFilmstrip
+        links: [
+            {
+                name: "Profile",
+                url: "/profile/",
+                icon: mdiAccountBox
+            },
+
+            {
+                name: "Preferences",
+                url: "/preferences/",
+                icon: mdiCog
+            },
+
+            {
+                name: "History",
+                url: "/history/",
+                icon: mdiFilmstrip
+            },
+        ]
     }),
     components: {
-        Logo
+        Logo,
+        NavDrawerLinks
     }
 };
--- pickee_frontend/src/components/ProfileNavDrawer/ProfileNavDrawer.vue
+++ pickee_frontend/src/components/ProfileNavDrawer/ProfileNavDrawer.vue
@@ -1,21 +1,7 @@
 <template>
     <v-navigation-drawer class="nav-drawer">
         <div class="nav-drawer__logo"><Logo /></div>
-        <div class="nav-drawer-links">
-            <a class="nav-drawer-links__item nav-drawer-links__item--active" href="/profile/">
-                <v-icon class="mr-3">{{ iconProfile }}</v-icon>
-                Profile
-            </a>
-            <a class="nav-drawer-links__item" href="/preferences/">
-                <v-icon class="mr-3">{{ iconPreferences }}</v-icon>
-                Preferences
-            </a>
-            <a class="nav-drawer-links__item" href="/history/">
-                <v-icon class="mr-3">{{ iconHistory }}</v-icon>
-                History
-            </a>
-        </div>
-
+        <NavDrawerLinks :links="links" />
         <div class="nav-drawer__logout-btn">
             <v-btn block>Logout</v-btn>
         </div>
