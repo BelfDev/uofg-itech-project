5f0c2bb0ef5374db0f6e00996f472efa1323756d
Rhys <rhys-stephens@hotmail.co.uk>
2020-03-14 11:13:40 +0000

    Created new user model based on project requirements

--- pickee_api/models.py
+++ pickee_api/models.py
@@ -1,9 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 from django.core.validators import MinValueValidator, MaxValueValidator
 
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+class UserProfile(AbstractUser):
+    email = models.EmailField(max_length=300, unique=True, blank=False)
+    first_name = models.CharField(max_length=36, blank=False)
+    last_name = models.CharField(max_length=36, blank=False)
 
     picture = models.ImageField(upload_to='profile_images', blank=True)
     avatar = models.CharField(max_length=500)
