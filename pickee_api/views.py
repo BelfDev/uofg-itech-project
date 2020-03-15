f53185e8085222a27b1d146206aaaa44776f9941
Pedro Belfort <belfortdev@gmail.com>
2020-03-15 19:26:59 +0000

    Optimize imports

--- pickee_api/views.py
+++ pickee_api/views.py
@@ -1,7 +1,6 @@
 import json
 
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 
 # Temporary renders login.html
