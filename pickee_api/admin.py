d5fbc05e730305be84849f2c4fd7bcc9f896b290
Pedro Belfort <belfortdev@gmail.com>
2020-03-15 00:28:03 +0000

    Customize model appearance on admin site

--- pickee_api/admin.py
+++ pickee_api/admin.py
@@ -1,8 +1,26 @@
 from django.contrib import admin
+
 from pickee_api import models
 
+
+@admin.register(models.UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['id', 'email', 'first_name']
+    list_editable = ['email', 'first_name']
+    ordering = ['id']
+
+
+@admin.register(models.Session)
+class SessionAdmin(admin.ModelAdmin):
+    list_display = ['id']
+
+
+@admin.register(models.Recommendation)
+class RecommendationAdmin(admin.ModelAdmin):
+    list_display = ['id', 'movie', 'user_choice']
+
+
 # Registers Pickee API models in the admin site
-admin.site.register(models.UserProfile)
 admin.site.register(models.FavoriteActor)
 admin.site.register(models.Actor)
 admin.site.register(models.FavoriteMovie)
@@ -10,5 +28,3 @@ admin.site.register(models.Movie)
 admin.site.register(models.MovieCast)
 admin.site.register(models.FavoriteGenre)
 admin.site.register(models.Genre)
-admin.site.register(models.Recommendation)
-admin.site.register(models.Session)
