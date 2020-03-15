558c220a5e01b0394e47592cdb28467a05bea4b9
Pedro Belfort <belfortdev@gmail.com>
2020-03-15 00:28:18 +0000

    Register root api URLs

--- pickee_api/urls.py
+++ pickee_api/urls.py
@@ -6,25 +6,25 @@ from pickee_api.views import auth, profile, recommendation
 app_name = 'pickee_api'
 
 # Routers provide an easy way of automatically determining the URL conf.
-router = routers.DefaultRouter()
-router.register(r'users', auth.UserViewSet)
+## Main Router ('api/$')
+rootRouter = routers.DefaultRouter()
+rootRouter.register(r'users', auth.UserViewSet)
+rootRouter.register(r'profiles', profile.UserProfileViewSet)
+rootRouter.register(r'actors', profile.ActorViewSet)
+rootRouter.register(r'movies', profile.MovieViewSet)
+rootRouter.register(r'movie-cast', profile.MovieCastViewSet)
+rootRouter.register(r'genres', profile.GenreViewSet)
+rootRouter.register(r'recommendations', recommendation.RecommendationViewSet)
+rootRouter.register(r'sessions', recommendation.SessionViewSet)
 
-router.register(r'profile', profile.UserProfileViewSet)
-router.register(r'profile/favorite-actor', profile.FavoriteActorViewSet)
-router.register(r'profile/actor', profile.ActorViewSet)
-router.register(r'profile/favorite-movie', profile.FavoriteMovieViewSet)
-router.register(r'profile/movie', profile.MovieViewSet)
-router.register(r'profile/movie-cast', profile.MovieCastViewSet)
-router.register(r'profile/movie', profile.MovieViewSet)
-router.register(r'profile/favorite-genre', profile.FavoriteGenreViewSet)
-router.register(r'profile/genre', profile.GenreViewSet)
-
-router.register(r'recommendation', recommendation.RecommendationViewSet)
-router.register(r'recommendation/session', recommendation.SessionViewSet)
+# ## Profile Router ('api/profile/$')
+# profileRouter = routers.DefaultRouter()
+# profileRouter.register(r'favorite-actor', profile.FavoriteActorViewSet)
+# profileRouter.register(r'favorite-genre', profile.FavoriteGenreViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', include(rootRouter.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
