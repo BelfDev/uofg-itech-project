6a6ff9b3c685b165a952b5ef8a65f266d12c406a
Pedro Belfort <belfortdev@gmail.com>
2020-03-15 19:37:01 +0000

    Enforce authentication in api endpoints

--- pickee_api/api.py
+++ pickee_api/api.py
@@ -9,38 +9,43 @@ from pickee_api.serializers import PickeeUserSerializer
 class PickeeUserViewSet(viewsets.ModelViewSet):
     queryset = PickeeUser.objects.all().order_by('-date_joined')
     serializer_class = PickeeUserSerializer
-    # permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class FavoriteActorViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
     queryset = models.FavoriteActor.objects.all()
     serializer_class = serializers.FavoriteActorSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class ActorViewSet(viewsets.ModelViewSet):
     queryset = models.Actor.objects.all()
     serializer_class = serializers.ActorSerializer
-    # permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class FavoriteMovieViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
     queryset = models.FavoriteMovie.objects.all()
     serializer_class = serializers.FavoriteMovieSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = models.Movie.objects.all()
     serializer_class = serializers.MovieSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class MovieCastViewSet(viewsets.ModelViewSet):
     queryset = models.MovieCast.objects.all()
     serializer_class = serializers.MovieCastSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class FavoriteGenreViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
     queryset = models.FavoriteGenre.objects.all()
     serializer_class = serializers.FavoriteGenreSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class GenreViewSet(viewsets.ModelViewSet):
@@ -51,8 +56,10 @@ class GenreViewSet(viewsets.ModelViewSet):
 class RecommendationViewSet(viewsets.ModelViewSet):
     queryset = models.Recommendation.objects.all()
     serializer_class = serializers.RecommendationSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class SessionViewSet(viewsets.ModelViewSet):
     queryset = models.Session.objects.all()
     serializer_class = serializers.SessionSerializer
+    permission_classes = [permissions.IsAuthenticated]
