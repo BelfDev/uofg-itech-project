605b2ff07ec6c7ea10ad66b88b06112c541dfbc6
Pedro Belfort <belfortdev@gmail.com>
2020-03-15 00:51:18 +0000

    Postpone nested routes

--- pickee_api/urls.py
+++ pickee_api/urls.py
@@ -6,7 +6,7 @@ from pickee_api.views import auth, profile, recommendation
 app_name = 'pickee_api'
 
 # Routers provide an easy way of automatically determining the URL conf.
-## Main Router ('api/$')
+# - Main Router ('api/$')
 rootRouter = routers.DefaultRouter()
 rootRouter.register(r'users', auth.UserViewSet)
 rootRouter.register(r'profiles', profile.UserProfileViewSet)
@@ -17,14 +17,16 @@ rootRouter.register(r'genres', profile.GenreViewSet)
 rootRouter.register(r'recommendations', recommendation.RecommendationViewSet)
 rootRouter.register(r'sessions', recommendation.SessionViewSet)
 
-# ## Profile Router ('api/profile/$')
+# TODO: Finish the nested routes
+# - Profile Router ('api/profile/$')
 # profileRouter = routers.DefaultRouter()
-# profileRouter.register(r'favorite-actor', profile.FavoriteActorViewSet)
-# profileRouter.register(r'favorite-genre', profile.FavoriteGenreViewSet)
+# profileRouter.register(r'favorite-actors', profile.FavoriteActorViewSet)
+# profileRouter.register(r'favorite-genres', profile.FavoriteGenreViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(rootRouter.urls)),
+    # path('profiles/<int:pk>/', include(profileRouter.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
