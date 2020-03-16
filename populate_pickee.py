46a9da2243a7bd6e759609aa564de063a5ba65d2
Rhys <rhys-stephens@hotmail.co.uk>
2020-03-16 15:10:25 +0000

    Fixed bugs that were preventing script from running

--- populate_pickee.py
+++ populate_pickee.py
@@ -8,6 +8,7 @@ from pickee_api.models import (PickeeUser,FavoriteActor,Actor,
                                 FavoriteMovie,Movie,MovieCast,
                                 FavoriteGenre,Genre,Recommendation,
                                 Session)
+from pickee_api.managers import PickeeUserManager
 
 users = [
     {'email':'rhys@email.com','password':'rhyspassword','first_name':'Rhys','last_name':'Stephens',
@@ -45,31 +46,31 @@ favorite_movies = [
     {'email':'pedro@email.com','movie_id':10184},
     {'email':'pedro@email.com','movie_id':102651},
     {'email':'nathan@email.com','movie_id':10184},
-    {'email':'anton@email.com','movie_id':26205},
+    {'email':'anton@email.com','movie_id':27205},
     {'email':'anton@email.com','movie_id':278},
     {'email':'anton@email.com','movie_id':284053},
 ]
 
 movies = [
     {'id':301528,'name':'Toy Story 4','rating':7.5,'release_date':datetime.date(2019,6,19),
-        'decription':'''Woody has always been confident about his place in the world and that his priority is taking 
+        'description':'''Woody has always been confident about his place in the world and that his priority is taking 
         care of his kid, whether that's Andy or Bonnie. But when Bonnie adds a reluctant new toy called \"Forky\" 
         to her room, a road trip adventure alongside old and new friends will show Woody how big the world can be 
         for a toy.'''},
     {'id':27205,'name':'Inception','rating':8.3,'release_date':datetime.date(2010,7,15),
-        'decription':'''Cobb, a skilled thief who commits corporate espionage by infiltrating the subconscious of 
+        'description':'''Cobb, a skilled thief who commits corporate espionage by infiltrating the subconscious of 
         his targets is offered a chance to regain his old life as payment for a task considered to be impossible: 
         \"inception\", the implantation of another person's idea into a target's subconscious.'''},
     {'id':284053,'name':'Thor:Ragnarok','rating':7.5,'release_date':datetime.date(2017,10,25),
-        'decription':'''Thor is imprisoned on the other side of the universe and finds himself in a race against 
+        'description':'''Thor is imprisoned on the other side of the universe and finds himself in a race against 
         time to get back to Asgard to stop Ragnarok, the destruction of his home-world and the end of Asgardian 
         civilization, at the hands of an all-powerful new threat, the ruthless Hela.'''},
     {'id':10184,'name':'He\'s Just Not That Into You','rating':6.4,'release_date':datetime.date(2009,2,6),
-        'decription':'''Remember that really cute girl/guy who said they'd call – and didn't? Maybe they lost your 
+        'description':'''Remember that really cute girl/guy who said they'd call – and didn't? Maybe they lost your 
         number. Maybe they're in the hospital. Maybe they're awed by your looks, brains or success. Or maybe... 
         They're just not that into you.'''},
     {'id':102651,'name':'Maleficent','rating':7.1,'release_date':datetime.date(2014,5,28),
-        'decription':'''A beautiful, pure-hearted young woman, Maleficent has an idyllic life growing up in a 
+        'description':'''A beautiful, pure-hearted young woman, Maleficent has an idyllic life growing up in a 
         peaceable forest kingdom, until one day when an invading army threatens the harmony of the land.  Maleficent 
         rises to be the land's fiercest protector, but she ultimately suffers a ruthless betrayal – an act that 
         begins to turn her heart into stone. Bent on revenge, Maleficent faces an epic battle with the invading 
@@ -77,21 +78,21 @@ movies = [
         Maleficent realizes that Aurora holds the key to peace in the kingdom – and to Maleficent's true happiness 
         as well.'''},
     {'id':114150,'name':'Pitch Perfect','rating':7.3,'release_date':datetime.date(2012,9,28),
-        'decription':'''College student Beca knows she does not want to be part of a clique, but that's exactly 
+        'description':'''College student Beca knows she does not want to be part of a clique, but that's exactly 
         where she finds herself after arriving at her new school. Thrust in among mean gals, nice gals and just 
         plain weird gals, Beca finds that the only thing they have in common is how well they sing together. She 
         takes the women of the group out of their comfort zone of traditional arrangements and into a world of 
         amazing harmonic combinations in a fight to the top of college music competitions.'''},
     {'id':278,'name':'The Shawshank Redemption','rating':8.7,'release_date':datetime.date(1994,9,23),
-        'decription':'''Framed in the 1940s for the double murder of his wife and her lover, upstanding banker Andy 
+        'description':'''Framed in the 1940s for the double murder of his wife and her lover, upstanding banker Andy 
         Dufresne begins a new life at the Shawshank prison, where he puts his accounting skills to work for an 
         amoral warden. During his long stretch in prison, Dufresne comes to be admired by the other inmates -- 
         including an older prisoner named Red -- for his integrity and unquenchable sense of hope.'''},
     {'id':5255,'name':'The Polar Express','rating':6.6,'release_date':datetime.date(2004,11,10),
-        'decription':'''When a doubting young boy takes an extraordinary train ride to the North Pole, he embarks on 
+        'description':'''When a doubting young boy takes an extraordinary train ride to the North Pole, he embarks on 
         a journey of self-discovery that shows him that the wonder of life never fades for those who believe.'''},
     {'id':32407,'name':'Malice In Wonderland','rating':5.7,'release_date':datetime.date(2009,1,1),
-        'decription':'''A modern take on the classic fairytale, Alice in Wonderland, set in South East England.An 
+        'description':'''A modern take on the classic fairytale, Alice in Wonderland, set in South East England.An 
         American law student in London. Knocked down by a black cab, she wakes with amnesia in a world that's a 
         million miles from home - Wonderland. We follow her adventures as she's dragged through an underworld filled 
         with twisted individuals and the lowest low-lifers, by the enigmatic cab driver, Whitey. She needs to find 
@@ -99,10 +100,10 @@ movies = [
         progresses she discovers nothing is what it seems, realizes that fate and life are terminally entwined, and 
         finds true love lurking in the unlikeliest place.'''},
     {'id':8999,'name':'Derailed','rating':6.2,'release_date':datetime.date(2005,11,11),
-        'decription':'''When two married business executives having an affair are blackmailed by a violent criminal, 
+        'description':'''When two married business executives having an affair are blackmailed by a violent criminal, 
         they are forced to turn the tables on him to save their families.'''},
     {'id':393624,'name':'Official Secrets','rating':7.3,'release_date':datetime.date(2019,8,30),
-        'decription':'''The true story of British intelligence whistleblower Katharine Gun who—prior to the 2003 
+        'description':'''The true story of British intelligence whistleblower Katharine Gun who—prior to the 2003 
         Iraq invasion—leaked a top-secret NSA memo exposing a joint US-UK illegal spying operation against members 
         of the UN Security Council. The memo proposed blackmailing member states into voting for war.'''}
 ]
@@ -129,25 +130,25 @@ favorite_genres = [
 ]
 
 genres = [
-    {'genre_id':28,'name':'Action'},
-    {'genre_id':12,'name':'Adventure'},
-    {'genre_id':16,'name':'Animation'},
-    {'genre_id':35,'name':'Comedy'},
-    {'genre_id':80,'name':'Crime'},
-    {'genre_id':99,'name':'Documentary'},
-    {'genre_id':18,'name':'Drama'},
-    {'genre_id':10751,'name':'Family'},
-    {'genre_id':14,'name':'Fantasy'},
-    {'genre_id':36,'name':'History'},
-    {'genre_id':27,'name':'Horror'},
-    {'genre_id':10402,'name':'Music'},
-    {'genre_id':9648,'name':'Mystery'},
-    {'genre_id':10749,'name':'Romance'},
-    {'genre_id':878,'name':'Science Fiction'},
-    {'genre_id':10770,'name':'TV Movie'},
-    {'genre_id':53,'name':'Thriller'},
-    {'genre_id':10752,'name':'War'},
-    {'genre_id':37,'name':'Western'}
+    {'id':28,'name':'Action'},
+    {'id':12,'name':'Adventure'},
+    {'id':16,'name':'Animation'},
+    {'id':35,'name':'Comedy'},
+    {'id':80,'name':'Crime'},
+    {'id':99,'name':'Documentary'},
+    {'id':18,'name':'Drama'},
+    {'id':10751,'name':'Family'},
+    {'id':14,'name':'Fantasy'},
+    {'id':36,'name':'History'},
+    {'id':27,'name':'Horror'},
+    {'id':10402,'name':'Music'},
+    {'id':9648,'name':'Mystery'},
+    {'id':10749,'name':'Romance'},
+    {'id':878,'name':'Science Fiction'},
+    {'id':10770,'name':'TV Movie'},
+    {'id':53,'name':'Thriller'},
+    {'id':10752,'name':'War'},
+    {'id':37,'name':'Western'}
 ]
 
 recommendations = [
@@ -178,7 +179,7 @@ def populate():
         add_genre(genre['id'],genre['name'])
     
     for actor in favorite_actors:
-        add_favorite_actor(actor['email']),actor['actor_id']
+        add_favorite_actor(actor['email'],actor['actor_id'])
     
     for movie in favorite_movies:
         add_favorite_movie(movie['email'],movie['movie_id'])
@@ -194,12 +195,6 @@ def populate():
     
     for recommendation in recommendations:
         add_recommendation(recommendation['movie_id'],recommendation['session_id'],recommendation['user_choice'])
-    
-    for user in PickeeUser.objects.all():
-        for actor in Actor.objects.filter(user=user):
-            print(f'- {user}: {actor}')
-        for movie in Movie.objects.filter(user=user):
-            print(f'- {user}: {movie}')
 
 def add_user(email,password,first_name,last_name,gender,age):
     user = PickeeUser.objects.create_user(email=email,
@@ -241,7 +236,7 @@ def add_favorite_genre(email,genre_id):
 def add_session(users):
     session = Session.objects.create()
     for user_email in users:
-        session.users.add(user=PickeeUser.objects.get(email=user_email))
+        session.users.add(PickeeUser.objects.get(email=user_email))
 
 def add_recommendation(movie_id,session_id,user_choice):
     recommendation = Recommendation.objects.get_or_create(movie=Movie.objects.get(id=movie_id),
