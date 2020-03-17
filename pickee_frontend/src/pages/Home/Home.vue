ae690d8141be8c28324409b5c6b8f5dc6402525e
Anton Samoilov <absamoilov88@gmail.com>
2020-03-17 12:55:39 +0000

    Move content to the top

--- pickee_frontend/src/pages/Home/Home.vue
+++ pickee_frontend/src/pages/Home/Home.vue
@@ -2,22 +2,27 @@
     <v-app>
         <div class="page-home">
             <v-content>
+                <div class="page-home-content">
+                    <PageHeader />
+                </div>
+                <div class="page-home-content-bg"></div>
                 <div class="stripe stripe--dark"></div>
                 <div class="stripe stripe--light"></div>
                 <div class="stripe stripe--lighter"></div>
-                <div class="page-home-content">
-                    <p>Home</p>
-                </div>
             </v-content>
         </div>
     </v-app>
 </template>
 
 <script>
+    import PageHeader from '@/components/PageHeader/PageHeader.vue';
+
     export default {
         name: "Home",
         data: () => ({}),
         methods: {},
-        components: {}
+        components: {
+            PageHeader
+        }
     };
 </script>
