251f8ed713d1c5c53dd68564799d4e635a1deba8
Anton Samoilov <absamoilov88@gmail.com>
2020-03-17 14:18:34 +0000

    Update home page markup and styles

--- pickee_frontend/src/pages/Home/Home.vue
+++ pickee_frontend/src/pages/Home/Home.vue
@@ -2,13 +2,22 @@
     <v-app>
         <div class="page-home">
             <v-content>
-                <div class="page-home-content">
-                    <PageHeader />
+                <div class="page-home-screen page-home-screen--1 page-home-screen--active">
+                    <div class="page-home-content">
+                        <PageHeader />
+                        <Intro />
+                    </div>
+                </div>
+                <div class="page-home-screen page-home-screen--2">
+                    <div class="page-home-content">
+                        <PageHeader />
+                        <div>Form</div>
+                    </div>
                 </div>
                 <div class="page-home-content-bg"></div>
-                <div class="stripe stripe--dark"></div>
                 <div class="stripe stripe--light"></div>
                 <div class="stripe stripe--lighter"></div>
+                <div class="stripe stripe--dark"></div>
             </v-content>
         </div>
     </v-app>
@@ -16,13 +25,15 @@
 
 <script>
     import PageHeader from '@/components/PageHeader/PageHeader.vue';
+    import Intro from '@/components/Intro/Intro.vue';
 
     export default {
         name: "Home",
         data: () => ({}),
         methods: {},
         components: {
-            PageHeader
+            PageHeader,
+            Intro
         }
     };
 </script>
