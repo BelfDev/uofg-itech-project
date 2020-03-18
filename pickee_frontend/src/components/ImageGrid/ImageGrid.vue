da22dff2e153fe13511c0dd6a1f7d2afd2bcc0b4
nathanieldschneider <nathanieldanielschneider@gmail.com>
2020-03-18 21:52:59 +0000

    Add image grid component

---
+++ pickee_frontend/src/components/ImageGrid/ImageGrid.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="image-grid">
+        <h3 class="image-grid-header">Favorite Actors</h3>
+        <v-card class="image-grid-card" dark>
+            <v-card-text>
+                <div class="image-grid-content">
+                    <div class="image-grid-content__block">
+                        <v-avatar class="header-auth" size="100">
+                            <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John"/>
+                        </v-avatar>
+                        <p class="image-grid-content__label">Johnny Depp</p>
+                    </div>
+
+                    <div class="image-grid-content__block">
+                        <v-avatar class="header-auth" size="100">
+                            <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John"/>
+                        </v-avatar>
+                        <p class="image-grid-content__label">Johnny Depp</p>
+                    </div>
+
+                    <div class="image-grid-content__block">
+                        <v-avatar class="header-auth" size="100">
+                            <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John"/>
+                        </v-avatar>
+                        <p class="image-grid-content__label">Johnny Depp</p>
+                    </div>
+                </div>
+
+                <div class="image-grid-content__btn">
+                    <v-btn color="primary">Add actor</v-btn>
+                </div>
+             
+            </v-card-text>
+        </v-card>
+    </div>
+</template>
+
+<script src="./ImageGrid.js" />
+<style src="./ImageGrid.css" />
