e4b01e37b7f4d57f1ae994ea3c40c1bf9f6da854
Anton Samoilov <absamoilov88@gmail.com>
2020-03-18 17:52:56 +0000

    Add recommendation box component

---
+++ pickee_frontend/src/components/RecommendationBox/RecommendationBox.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="recommendation-box">
+        <RecommendationCarousel />
+        <RecommendationDescr />
+    </div>
+</template>
+
+<script src="./RecommendationBox.js" />
+<style src="./RecommendationBox.css" />
