7f5b6d913f45d64c282f04a3f8824a8fb2e02243
Anton Samoilov <absamoilov88@gmail.com>
2020-03-19 23:04:17 +0000

    Update header auth component

--- pickee_frontend/src/components/HeaderAuth/HeaderAuth.js
+++ pickee_frontend/src/components/HeaderAuth/HeaderAuth.js
@@ -1,9 +1,11 @@
-import { mdiClose } from "@mdi/js";
+import { mdiClose, mdiAccount } from "@mdi/js";
 
 export default {
     name: "HeaderAuth",
+    props: ['user'],
     data: () => ({
         overlay: false,
+        iconUser: mdiAccount,
         iconClose: mdiClose
     })
 }
