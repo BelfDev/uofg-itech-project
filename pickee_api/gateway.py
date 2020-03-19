80f2b48a992cb2911ed4a0791ab9ae5afe4f20c8
Pedro Belfort <belfortdev@gmail.com>
2020-03-19 18:41:52 +0000

    Extract utelly api key

--- pickee_api/gateway.py
+++ pickee_api/gateway.py
@@ -6,6 +6,7 @@ from pickee_api.utils import BearerAuth
 # The token was kept here to simplify the marking process
 # In a real-world scenario we would never commit this token
 TMDB_ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NjZjYTNlNDMxOWE4MzVjYWVlODI2MmE3YTgzZjNiNCIsInN1YiI6IjVlNjUwMDM3MTUxYzVjMDAxNWZmYWYyMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.iLqC8qnysp7u5Ha5ChC1Ag8iiLQP9zyWikURmHH7L48"
+UTELLY_API_KEY = "ed03a46877msh52c7837560678fep10a146jsnaac521c60402"
 
 
 def tmdb_example_endpoint(request):
@@ -19,7 +20,7 @@ def tmdb_example_endpoint(request):
 def utelly_example_endpoint(request):
     if request.method == 'GET':
         url = 'https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup'
-        headers = {"x-rapidapi-key": "ed03a46877msh52c7837560678fep10a146jsnaac521c60402"}
+        headers = {"x-rapidapi-key": UTELLY_API_KEY}
         params = {"term": "bojack", "country": "uk"}
         response = requests.get(url, headers=headers, params=params)
         data = response.json()
