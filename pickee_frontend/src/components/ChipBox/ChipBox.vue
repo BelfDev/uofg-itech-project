994e8ed9d693e6e416c098bab0da6c989e4edc94
nathanieldschneider <nathanieldanielschneider@gmail.com>
2020-03-20 21:10:41 +0000

    Create chip box component

---
+++ pickee_frontend/src/components/ChipBox/ChipBox.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="chip-box">
+        <h3 class="chip-box__header">
+            Top Genres
+        </h3>
+           <v-card class="chip-box-card" dark>
+            <v-card-text class="chip-box-card__content">
+                <v-chip class="chip-box-card__chip" color="primary" :close="true">
+                    Horror
+                </v-chip>
+            </v-card-text>
+        </v-card>
+    </div>
+</template>
+
+<script src="./ChipBox.js" />
+<style src="./ChipBox.css" />
