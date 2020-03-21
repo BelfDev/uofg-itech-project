3b64467274b7870140be4e47e38e8257988ad550
Anton Samoilov <absamoilov88@gmail.com>
2020-03-21 17:48:48 +0000

    Add methods for profile history integration

--- pickee_frontend/src/pages/ProfileHistory/ProfileHistory.vue
+++ pickee_frontend/src/pages/ProfileHistory/ProfileHistory.vue
@@ -6,7 +6,7 @@
                 <PageHeader />
                 <v-content class="profile-page-content">
                     <div class="profile-page-blocks">
-                        <ProfileHistoryList />
+                        <ProfileHistoryList :historyItems="historyItems" />
                     </div>
                 </v-content>
             </main>
@@ -18,12 +18,41 @@
     import PageHeader from "@/components/PageHeader/PageHeader.vue";
     import ProfileNavDrawer from "@/components/ProfileNavDrawer/ProfileNavDrawer.vue";
     import ProfileHistoryList from "@/components/ProfileHistoryList/ProfileHistoryList.vue";
+    import { mdiThumbDown, mdiThumbUp, mdiBookmark } from '@mdi/js';
+    import http from "@/services/http";
+
+    const choiceIconMap = {
+        "REJECTED": {
+            icon: mdiThumbDown,
+            color: 'red'
+        },
+        "APPROVED": {
+            icon: mdiThumbUp,
+            color: 'green'
+        },
+        "BOOKMARKED": {
+            icon: mdiBookmark,
+            color: 'primary'
+        }
+    }
+
     export default {
         name: "ProfileHistory",
         data: function() {
-            return {};
+            return {
+                historyItems: []
+            };
         },
         components: { PageHeader, ProfileNavDrawer, ProfileHistoryList },
+        created: async function() {
+            const historyResponse = await http.getRecommendationHistory();
+            this.historyItems = historyResponse.map(item => ({
+                image: item.image,
+                text: item.text,
+                icon: choiceIconMap[item.user_choice].icon,
+                color: choiceIconMap[item.user_choice].color
+            }))
+        },
         beforeMount() {
             const appElement = document.getElementsByTagName('app')[0];
             const data = appElement.getAttribute('data');
