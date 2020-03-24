3542cb9e8af44d0ad3d252c4324fc9c29d4b9262
Anton Samoilov <absamoilov88@gmail.com>
2020-03-24 17:19:18 +0000

    Update profile page

--- pickee_frontend/src/pages/Profile/Profile.vue
+++ pickee_frontend/src/pages/Profile/Profile.vue
@@ -7,8 +7,16 @@
                 <v-content class="profile-page-content">
                     <ProfileHeader :user="data.user" />
                     <div class="profile-page-blocks">
-                        <ProfilePersonalDetails :updatePersonalDetails="updatePersonalDetails" :user="data.user" class="mt-12" />
-                        <ProfileFriends :removeFriend="removeFriend" :items="friendsItems" class="mt-12" />
+                        <ProfilePersonalDetails 
+                            class="mt-12"
+                            :updatePersonalDetails="updatePersonalDetails" 
+                            :user="data.user" 
+                        />
+                        <ProfileFriends 
+                            class="mt-12"
+                            :removeFriend="removeFriend" 
+                            :items="friendsItems" 
+                        />
                     </div>
                 </v-content>
             </main>
@@ -17,7 +25,7 @@
 </template>
 
 <script>
-    import http from "@/services/http";
+    import api from "@/services/api";
     import PageHeader from "@/components/PageHeader/PageHeader.vue";
     import ProfileNavDrawer from "@/components/ProfileNavDrawer/ProfileNavDrawer.vue";
     import ProfileHeader from "@/components/ProfileHeader/ProfileHeader.vue";
@@ -29,7 +37,9 @@
         name: "Profile",
         data: function() {
             return {
-                friendsItems: []
+                friendsItems: [],
+                friendsIDs: [],
+                userID: null,
             };
         },
         components: { 
@@ -40,35 +50,38 @@
             ProfileFriends
         },
         methods: {
-            removeFriend: function(targetItem) {
-                console.log('removeFriend', targetItem.text);
-                const response = http.removeFriend();
-                console.log(response);
+            removeFriend: async function(friend) {
+                const targetItemIndex = this.friendsItems.findIndex(sourceItem => sourceItem.text === friend.text);
+                this.friendsItems.splice(targetItemIndex, 1);
+                this.friendsIDs.splice(targetItemIndex, 1);
 
-                // if(response.status === 200) {
-                    const targetItemIndex = this.friendsItems.findIndex(sourceItem => sourceItem.text === targetItem.text);
-                    this.friendsItems.splice(targetItemIndex, 1);
-                // }
+                await api.removeFriend(this.userID, this.friendsIDs);
             },
-            updatePersonalDetails: function(data) {
-                console.log('updatePersonalDetails', data);
-                const response = http.updateUserProfile(data);
-                console.log(response);
+            updatePersonalDetails: async function(name, value) {
+                const params = {};
+                params[name] = value;
+                await api.updateUserProfile(this.userID, params);
             }
         },
         mounted: async function() {
-            const friendsResponse = await http.getFriends();
-            this.friendsItems = friendsResponse.map(friend => ({
-                image: friend.picture,
-                text: `${friend.first_name} ${friend.last_name}`,
-                icon: mdiMinusCircle
-            }));
+            const friendsResponse = await api.getFriends(this.userID);
+            this.friendsItems = friendsResponse.map(friend => {
+                this.friendsIDs.push(friend.id);
+                return {
+                    id: friend.id,
+                    image: friend.picture,
+                    text: `${friend.first_name} ${friend.last_name}`,
+                    icon: mdiMinusCircle
+                }
+            });
+            console.log(this.friendsIDs);
         },
         beforeMount() {
             const appElement = document.getElementsByTagName('app')[0];
-            const data = appElement.getAttribute('data');
+            let data = appElement.getAttribute('data');
             if (data) {
                 this.data = JSON.parse(data);
+                this.userID = this.data.user.id;
             }
         }
     };
