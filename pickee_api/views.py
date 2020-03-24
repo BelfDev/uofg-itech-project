87b854063f5dd7846c4ce04ec2133e68cf660fdb
Pedro Belfort <belfortdev@gmail.com>
2020-03-24 15:01:51 -0300

    Add recommendation history

--- pickee_api/views.py
+++ pickee_api/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
 from pickee_api.forms import PickeeUserCreationForm
+from pickee_api.models import Recommendation
 
 
 def home(request):
@@ -18,7 +19,7 @@ def home(request):
 def recommendation(request):
     user = json.dumps(__get_user_data(request))
     preferences = json.dumps(request.POST)
-    context = { 'user': user, 'preferences': preferences}
+    context = {'user': user, 'preferences': preferences}
 
     return render(request, 'recommendation.html', context=context)
 
@@ -82,14 +83,34 @@ def profile(request):
     user_profile_data = {"age": request.user.age, "gender": request.user.gender}
     data = json.dumps({
         "user": {**__get_user_data(request), **user_profile_data}
-    });
+    })
     context = {'data': data}
     return render(request, 'profile.html', context=context)
 
 
 @login_required
 def history(request):
-    return render(request, 'history.html')
+    recommendations = __get_recommendation_history(request)
+    results = []
+    for recommendation in recommendations:
+        movie = recommendation.movie
+        result = {
+            'id': recommendation.id,
+            'user_choice': recommendation.user_choice,
+            'session_id': recommendation.session_id,
+            'movie': {
+                'id': movie.id,
+                'name': movie.name,
+                'image_url': movie.image_url
+            },
+        }
+        results.append(result)
+
+    data = json.dumps({
+        "results": results
+    })
+    context = {'data': data}
+    return render(request, 'history.html', context=context)
 
 
 @login_required
@@ -109,3 +130,15 @@ def __get_user_data(request):
         "email": request.user.email,
         "picture": request.user.picture.url if request.user.picture else None
     }
+
+
+def __get_recommendation_history(request):
+    user = request.user
+    if user:
+        # PickeeUser.objects.filter(session__id=1).values_list('id', flat=True)
+        # sessions = Session.objects.filter(users__in=user)
+        # sessions = PickeeUser.objects.get(id=user.id).session_set
+        sessions = user.session_set.all()
+        recommendations = Recommendation.objects.filter(session__in=sessions)
+        if recommendations:
+            return recommendations
