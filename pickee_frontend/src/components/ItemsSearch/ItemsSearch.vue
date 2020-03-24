1d48071958b726c9dbbdde2a94cd839bd5f6f463
Anton Samoilov <absamoilov88@gmail.com>
2020-03-24 16:46:43 +0000

    Add ItemsSearch component

---
+++ pickee_frontend/src/components/ItemsSearch/ItemsSearch.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="items-search">
+        <SearchSuggestions 
+            :entries="entries" 
+            :addMethod="addMethod" 
+            :searchMethod="searchMethod" 
+            :searchLoading="searchLoading" 
+        />
+        <ItemList :items="items" :button-action="removeMethod" />
+    </div>
+</template>
+
+<script src="./ItemsSearch.js" />
+<style src="./ItemsSearch.css" />
