b71dbb4e1472a374a85c5eb600585851ce36dc39
Anton Samoilov <absamoilov88@gmail.com>
2020-03-24 20:01:23 +0000

    Profile header picture check

--- pickee_frontend/src/components/ProfileHeader/ProfileHeader.vue
+++ pickee_frontend/src/components/ProfileHeader/ProfileHeader.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="profile-header">
         <v-avatar class="profile-header__avatar" size="19.2rem">
-            <img :src="user.picture" :alt="user.name" />
+            <img :src="user.picture" :alt="user.name" v-if="user.picture" />
+            <v-icon size="10rem" color="white" v-else>{{ iconUser }}</v-icon>
         </v-avatar>
         <div class="profile-header__content">
             <h2 class="profile-header__name">{{ user.name}}</h2>
