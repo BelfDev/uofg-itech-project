e4e0387f900483e6915d3ad24a8e0797269d06be
Rhys <rhys-stephens@hotmail.co.uk>
2020-03-25 13:33:33 +0000

    Test to ensure release date is in the past

--- pickee_api/tests.py
+++ pickee_api/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from pickee_api.models import Movie
+import datetime
 
 class MovieModelTest(TestCase):
     def test_ensure_rating_is_positive(self):
@@ -9,3 +10,7 @@ class MovieModelTest(TestCase):
     def test_ensure_rating_is_less_than_or_equal_to_ten(self):
         movie = Movie.objects.create(id=2,name='Test Movie',rating=6)
         self.assertTrue(movie.rating <= 10)
+    
+    def test_release_date_in_past(self):
+        movie = Movie.objects.create(id=3,name='Test Movie',rating=7,release_date=datetime.date(2019,11,12))
+        self.assertTrue(movie.release_date < datetime.date.today())
