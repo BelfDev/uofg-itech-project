2bf40ecbb0d28658571312c8de8fea6d4a2c5651
Rhys <rhys-stephens@hotmail.co.uk>
2020-03-25 13:05:25 +0000

    Test to ensure movie ratings are postive

--- pickee_api/tests.py
+++ pickee_api/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from pickee_api.models import Movie
 
-# Create your tests here.
+class MovieModelTest(TestCase):
+    def test_ensure_rating_is_positive(self):
+        movie = Movie.objects.create(id=1,name='Test Movie',rating=5)
+        movie.save()
+
+        self.assertEquals((movie.rating >= 0), True)
