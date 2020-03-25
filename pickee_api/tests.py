d235b2d614245cc60fcfec5be61c042a2ef2b822
Rhys <rhys-stephens@hotmail.co.uk>
2020-03-25 13:25:27 +0000

    Changed assert methods

--- pickee_api/tests.py
+++ pickee_api/tests.py
@@ -4,8 +4,8 @@ from pickee_api.models import Movie
 class MovieModelTest(TestCase):
     def test_ensure_rating_is_positive(self):
         movie = Movie.objects.create(id=1,name='Test Movie',rating=5)
-        self.assertEquals((movie.rating >= 0), True)
+        self.assertTrue(movie.rating >= 0)
     
     def test_ensure_rating_is_less_than_or_equal_to_ten(self):
         movie = Movie.objects.create(id=2,name='Test Movie',rating=6)
-        self.assertEquals((movie.rating <= 10), True)
+        self.assertTrue(movie.rating <= 10)
