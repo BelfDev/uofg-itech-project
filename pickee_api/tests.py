12670f9c6251174e21588e0e559a8f223916eee0
Rhys <rhys-stephens@hotmail.co.uk>
2020-03-25 13:20:55 +0000

    Removed save methods that were not required

--- pickee_api/tests.py
+++ pickee_api/tests.py
@@ -4,12 +4,8 @@ from pickee_api.models import Movie
 class MovieModelTest(TestCase):
     def test_ensure_rating_is_positive(self):
         movie = Movie.objects.create(id=1,name='Test Movie',rating=5)
-        movie.save()
-
         self.assertEquals((movie.rating >= 0), True)
     
     def test_ensure_rating_is_less_than_or_equal_to_ten(self):
         movie = Movie.objects.create(id=2,name='Test Movie',rating=6)
-        movie.save()
-
         self.assertEquals((movie.rating <= 10), True)
