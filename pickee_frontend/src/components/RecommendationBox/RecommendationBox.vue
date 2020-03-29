0d0a48cb51877cddf257e967e3757ebe613a26a0
Anton Samoilov <absamoilov88@gmail.com>
2020-03-29 23:44:32 +0100

    Fix button message

--- pickee_frontend/src/components/RecommendationBox/RecommendationBox.vue
+++ pickee_frontend/src/components/RecommendationBox/RecommendationBox.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="recommendation-box">
-        <v-btn color="secondary" class="recommendation-box__goback" href="/?step=2">Back to the preferences</v-btn>
+        <v-btn color="secondary" class="recommendation-box__goback" href="/?step=2">Back to preferences</v-btn>
         <div class="mx-auto">
             <v-progress-circular
                 :size="140"
