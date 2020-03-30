4c9def7c513740bc8e06fdc3dc4fb73a72358a56
Anton Samoilov <absamoilov88@gmail.com>
2020-03-30 12:43:25 +0100

    Disabled debug

--- pickee_project/settings.py
+++ pickee_project/settings.py
@@ -30,7 +30,7 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 SECRET_KEY = 'g(c-@a@knp%bqc(g&kz8c1+%nupy549vfic&m0(=xgangbb2a%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['madstax.pythonanywhere.com', '127.0.0.1']
 
